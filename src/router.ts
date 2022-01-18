cc2223c3fc42d3b47cf6bd6248ed11c74a1a7c9f
Fernando Monja Vasquez <fernandomonjav@gmail.com>
2022-01-18 12:38:07 -0500

    Update route error handling with exceptions

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response, Router } from 'express'
+import { HttpException } from './exceptions'
 import productRoute from './routes/product.route'
 
 const router = Router()
@@ -10,13 +11,20 @@ router.get('/', (req, res) => {
 router.use('/products', productRoute)
 
 router.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
+  if (error instanceof HttpException) {
+    res
+      .status(error.status)
+      .json({ message: error.message, status: error.status, error: error.error })
+    return
+  }
+
   if (process.env.NODE_ENV !== 'production') {
     console.log(error)
   }
 
   res
     .status(500)
-    .json({ status: 500, message: 'Something went wrong', error: 'Internal Server Error' })
+    .json({ message: 'Something went wrong', status: 500, error: 'Internal Server Error' })
 })
 
 export default router
